ac700fd04b99e0b665c40575edea78c4bc2be9ee
Venkata Goli (TT) <Venkata.Goli@tradingtechnologies.com>
2017-03-09 14:06:33 -0600

    test now

--- git_test.sh
+++ git_test.sh
@@ -4,17 +4,28 @@ set -b
 GIT_BRANCH=$1
 if [ $GIT_BRANCH ]; then
     # Merging to Master
-    echo "Merging to Branch $GIT_BRANCH to Master" 
+    echo "============ Merging to Branch $GIT_BRANCH to Master ============" 
+    echo "===== Checking out master ======="
     git checkout master
+    echo "===== Updating master ======="
     git pull
+    echo "===== Submodule update on develop ======="
     git submodule update
+    echo "===== Mergin $GIT_BRANCH to develop ======="
     git merge $GIT_BRANCH
+    echo "===== Pushing develop ======="
     git push origin master
 
     # Merging to Develop
+    echo " ============ Merging to Branch $GIT_BRANCH to Develop ============ " 
+     echo "===== Checking out develop ======="
     git checkout develop
+    echo "===== Updating develop ======="
     git pull
+    echo "===== Submodule update on develop ======="
     git submodule update
+    echo "===== Mergin $GIT_BRANCH to develop ======="
     git merge $GIT_BRANCH
+    echo "===== Pushing develop ======="
     git push origin develop
 fi
